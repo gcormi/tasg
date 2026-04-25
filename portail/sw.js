1aa177f1a5849610a6bb67167920be8450e8f7af
gcormi <gilles.cormi@gmail.com>
2026-04-25 07:55:59 +0200

    2504

--- portail/sw.js
+++ portail/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portail-ia-v2';
+const CACHE_NAME = 'portail-ia-v3';
 const ASSETS = [
   './index.html',
   './manifest.json',
@@ -11,12 +11,28 @@ self.addEventListener('install', (event) => {
       return cache.addAll(ASSETS);
     })
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
 });
 
 self.addEventListener('fetch', (event) => {
+  // Network first strategy for everything to always get the latest updates
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
     })
   );
 });
